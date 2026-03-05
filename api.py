103e7892e5296a2d0f70b9412b439370325cfca8
Kevin Gomez <nexus@Kevins-MacBook-Air.local>
2026-03-05 14:46:52 -0500

    feat: cambiando la consulta SQL para poder tener filtros dinámicos

--- api.py
+++ api.py
@@ -17,14 +17,12 @@ engine = create_engine('postgresql://nexus@localhost:5432/dota2_meta')
 
 @app.get("/api/rendimiento")
 def obtener_rendimiento():
+    # Ahora traemos TODA la información de la partida
     query = """
-    SELECT match_id, hero_name, kda, kda_rolling_avg 
-    FROM matches 
+    SELECT * FROM matches 
     ORDER BY match_id ASC
     """
     df = pd.read_sql(query, engine)
-    
-    # EL FIX: Reemplazar los 'NaN' por 0 para que JSON no falle
-    df = df.fillna(0)
+    df = df.fillna(0) # Limpiamos los vacíos
     
     return df.to_dict(orient="records")
